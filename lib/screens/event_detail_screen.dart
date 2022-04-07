abb4f8f7f3af6b18ad019c68ec4c8b2f4cd9ed01
Ömer Çiftci <omerrciftcii@hotmail.com>
2022-04-07 21:13:09 +0300

    Event detail screen has been fixed

--- lib/screens/event_detail_screen.dart
+++ lib/screens/event_detail_screen.dart
@@ -3,7 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:parti_app/widgets/custom_divider.dart';
 
+import '../ui_helpers/expandable_fab.dart';
+
 class EventDetailScreen extends StatelessWidget {
+  void _showAction(BuildContext context, int index) {
+    showDialog<void>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Text(''),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('CLOSE'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   const EventDetailScreen({Key? key}) : super(key: key);
 
   @override
@@ -28,7 +47,35 @@ class EventDetailScreen extends StatelessWidget {
           ),
         ),
       ),
+      floatingActionButton: ExpandableFab(
+        distance: 90.0,
+        children: [
+          ActionButton(
+            onPressed: () => _showAction(context, 0),
+            icon: const Icon(
+              Icons.person_add_alt_outlined,
+              color: Colors.white,
+            ),
+          ),
+          ActionButton(
+            onPressed: () => _showAction(context, 1),
+            icon: const Icon(
+              Icons.message,
+              color: Colors.white,
+            ),
+          ),
+          ActionButton(
+            onPressed: () => _showAction(context, 2),
+            icon: const Icon(
+              Icons.map,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           CachedNetworkImage(
               imageUrl:
@@ -36,81 +83,105 @@ class EventDetailScreen extends StatelessWidget {
           SizedBox(
             height: 10,
           ),
-          Text(
-            "Şamxal Bağışov's Home Party",
-            style: GoogleFonts.jost(fontWeight: FontWeight.w600, fontSize: 18),
+          Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: Text(
+              "Şamxal Bağışov's Home Party",
+              style:
+                  GoogleFonts.jost(fontWeight: FontWeight.w600, fontSize: 18),
+            ),
           ),
           SizedBox(
             height: 10,
           ),
-          Row(
-            children: [
-              Expanded(
-                child: ListTile(
-                  leading: Icon(Icons.calendar_today_outlined),
-                  title: Text(
-                    'Wednesday, April 12, 2022',
-                    style: GoogleFonts.jost(
-                        fontWeight: FontWeight.w500, fontSize: 16),
-                  ),
-                  subtitle: Text(
-                    '19:00 - 21:00 GMT+03:00',
-                    style: GoogleFonts.jost(
-                        fontWeight: FontWeight.w500, fontSize: 16),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          CustomDivider(),
-          Row(
-            children: [
-              Expanded(
-                child: ListTile(
-                  leading: Icon(Icons.location_on_sharp),
-                  title: Text(
-                    'Water Garden',
-                    style: GoogleFonts.jost(
-                        fontWeight: FontWeight.w500, fontSize: 16),
-                  ),
-                  subtitle: Text(
-                    'Barbaros, Kızılbegonya Sok 10/1',
-                    style: GoogleFonts.jost(
-                        fontWeight: FontWeight.w500, fontSize: 16),
+          Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    leading: Icon(Icons.calendar_today_outlined),
+                    title: Text(
+                      'Wednesday, April 12, 2022',
+                      style: GoogleFonts.jost(
+                          fontWeight: FontWeight.w500, fontSize: 16),
+                    ),
+                    subtitle: Text(
+                      '19:00 - 21:00 GMT+03:00',
+                      style: GoogleFonts.jost(
+                          fontWeight: FontWeight.w500, fontSize: 16),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          CustomDivider(),
-          Text(
-            'Party Owner',
-            style: GoogleFonts.jost(
-              fontWeight: FontWeight.bold,
+              ],
             ),
           ),
-          Row(
-            children: [
-              Text(
-                'Şamxal Bağışov',
-                style: GoogleFonts.jost(fontWeight: FontWeight.bold),
-              ),
-              CachedNetworkImage(
-                imageUrl: "https://i.postimg.cc/DwS9kKFX/Shmakha.png",
-                imageBuilder: (context, imageProvider) => Container(
-                  width: 140.0,
-                  height: 140.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: imageProvider, fit: BoxFit.cover),
+          CustomDivider(),
+          Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    leading: Icon(Icons.location_on_sharp),
+                    title: Text(
+                      'Water Garden',
+                      style: GoogleFonts.jost(
+                          fontWeight: FontWeight.w500, fontSize: 16),
+                    ),
+                    subtitle: Text(
+                      'Barbaros, Kızılbegonya Sok 10/1',
+                      style: GoogleFonts.jost(
+                          fontWeight: FontWeight.w500, fontSize: 16),
+                    ),
                   ),
                 ),
-                placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Icon(Icons.error),
+              ],
+            ),
+          ),
+          CustomDivider(),
+          Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: Text(
+              'Party Owner',
+              style: GoogleFonts.jost(
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          )
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    leading: CachedNetworkImage(
+                      imageUrl: "https://i.postimg.cc/DwS9kKFX/Shmakha.png",
+                      imageBuilder: (context, imageProvider) => Container(
+                        width: 40.0,
+                        height: 40.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: imageProvider, fit: BoxFit.cover),
+                        ),
+                      ),
+                      placeholder: (context, url) =>
+                          CircularProgressIndicator(),
+                      errorWidget: (context, url, error) => Icon(Icons.error),
+                    ),
+                    title: Text(
+                      'Şamxal Bağışov',
+                      style: GoogleFonts.jost(fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
